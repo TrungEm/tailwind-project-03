714aa30442543906fb95d6ec67e757d3ac5c1271
Trung Em <nguyentrungem2002@gmail.com>
2023-03-20 23:03:40 +0700

    Dựng base

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./public/**/*.{html,js}"],
+    theme: {
+        fontFamily: {
+            sans: ["Poppins", "sans-serif"],
+            Lato: ["Lato", "sans-serif"],
+        },
+        extend: {},
+    },
+    plugins: [],
+};
