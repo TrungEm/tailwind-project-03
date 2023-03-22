1436175c4d98a6aeda14f9a020adac4927b99b1a
Trung Em <nguyentrungem2002@gmail.com>
2023-03-22 16:25:22 +0700

    Làm phần hero #1

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,14 @@ module.exports = {
             sans: ["Poppins", "sans-serif"],
             Lato: ["Lato", "sans-serif"],
         },
-        extend: {},
+        extend: {
+            boxShadow: {
+                "3xl": "30px 40px 150px -10px rgba(0, 0, 0, 0.18)",
+                "4xl": "0px 0px 150px -20px rgba(0, 0, 0, 0.06)",
+                "5xl": "40px 50px 100px -30px rgba(28, 28, 28, 0.06)",
+                "6xl": "-14px -20px 110px -40px rgba(0, 0, 0, 0.14)",
+            },
+        },
     },
     plugins: [],
 };
