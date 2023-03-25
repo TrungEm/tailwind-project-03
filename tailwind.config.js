824057a200cd511e9b6d5235e391a28049fc8f9b
Trung Em <nguyentrungem2002@gmail.com>
2023-03-25 19:22:48 +0700

    Làm phần why us

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,7 @@ module.exports = {
                 "4xl": "0px 0px 150px -20px rgba(0, 0, 0, 0.06)",
                 "5xl": "40px 50px 100px -30px rgba(28, 28, 28, 0.06)",
                 "6xl": "-14px -20px 110px -40px rgba(0, 0, 0, 0.14)",
+                "7xl": "0px 25px 70px 10px rgba(0, 0, 0, 0.04)",
             },
         },
     },
